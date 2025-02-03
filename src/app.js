a318a4059112c3fa965d9c59681e5a657db1105a
prashant thakre <prashantthakre712@gmail.com>
2025-02-03 22:12:13 +0530

    Middlewares

--- src/app.js
+++ src/app.js
@@ -2,15 +2,66 @@ const express = require("express");
 
 const app = express();
 
-app.get("/",(req,res) =>
+// app.get("/user/:id/:name/:password",(req,res)=>
+// {
+//     console.log(req.params);
+//     res.send({
+//         firstname: "prashant",
+//         lastname: "thakre"
+//     })
+// })
+
+// app.get(/.*fly$/,(req,res)=>
+// {
+//     res.send({
+//               firstname: "prashant",
+//              lastname: "thakre"
+// })
+// })
+
+// app.get("/ab*c",(req,res)=>
+//     {
+//         res.send({
+//                   firstname: "prashant",
+//                  lastname: "thakre"
+//     })
+//     })
+
+app.use("/home",(req,res,next) =>
 {
-    res.send("This Is our home Page");
+    console.log("Middleware 1");
+    // res.send("First Response");
+    next();
+},
+(req,res,next)=>
+{
+    console.log("Middleware 2");
+    // res.send("Second Response");
+    next();
+},
+(req,res,next)=>
+{
+    console.log("Middleware 3");
+    // res.send("Third Response ");
+    next();
+},
+(req,res,next)=>{
+    console.log("Middleware 4");
+    // res.send("Fourth Response");
+    next();
+},
+(req,res,next)=>
+{
+    console.log("Middleware 5");
+    // res.send("Fifth Response");
+    next();
+},
+(req,res,next)=>
+{
+    console.log("Middleware 6");
+    res.send("Last Response ");
 });
 
-app.get("/about",(req,res)=>
-{
-    res.send("This Is About Page");
-})
 app.listen(8090 ,()=>
 {
     console.log("Your Server is Ready..... ");
